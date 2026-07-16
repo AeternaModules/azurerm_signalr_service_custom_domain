9bcffb2b21a29d69e6122b77c75d1b6f08be1052
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "signalr_service_custom_domains_id" {
   description = "Map of id values across all signalr_service_custom_domains, keyed the same as var.signalr_service_custom_domains"
-  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.id }
+  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "signalr_service_custom_domains_domain_name" {
   description = "Map of domain_name values across all signalr_service_custom_domains, keyed the same as var.signalr_service_custom_domains"
-  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.domain_name }
+  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.domain_name if v.domain_name != null && length(v.domain_name) > 0 }
 }
 output "signalr_service_custom_domains_name" {
   description = "Map of name values across all signalr_service_custom_domains, keyed the same as var.signalr_service_custom_domains"
-  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.name }
+  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "signalr_service_custom_domains_signalr_custom_certificate_id" {
   description = "Map of signalr_custom_certificate_id values across all signalr_service_custom_domains, keyed the same as var.signalr_service_custom_domains"
-  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.signalr_custom_certificate_id }
+  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.signalr_custom_certificate_id if v.signalr_custom_certificate_id != null && length(v.signalr_custom_certificate_id) > 0 }
 }
 output "signalr_service_custom_domains_signalr_service_id" {
   description = "Map of signalr_service_id values across all signalr_service_custom_domains, keyed the same as var.signalr_service_custom_domains"
-  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.signalr_service_id }
+  value       = { for k, v in azurerm_signalr_service_custom_domain.signalr_service_custom_domains : k => v.signalr_service_id if v.signalr_service_id != null && length(v.signalr_service_id) > 0 }
 }
 
